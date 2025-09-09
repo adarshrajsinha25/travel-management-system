c162a8e422dd93265362df357537a8145628fcf8
adarshrajsinha25 <adarshraj.288@gmail.com>
2025-09-09 16:12:08 +0530

    Parent directory

---
+++ travel-management-system/src/test/java/com/EaseTravel/travel_management_system/TravelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EaseTravel.travel_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
